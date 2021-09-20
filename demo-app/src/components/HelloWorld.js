cc1018227051a26f69e17f73070b23b16cd24a34
Eric Greene <eric@t4d.io>
2021-09-20 14:48:34 -0400

    add exercise 1

---
+++ demo-app/src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+
+
+
+export const HelloWorld = () => {
+  // return createElement(Fragment, null,
+  //   createElement('h1', null, 'Hello, World!'),
+  //   createElement('span', null, 'test'));
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <span>test</span>
+    </>
+  );
+};
