a780ad915f3f7744ab0067bb4e13b51d1d1b58fb
Eric Greene <eric@t4d.io>
2021-09-20 18:20:14 -0400

    add exercise 4

---
+++ demo-app/exercises/exercise-4.md
@@ -0,0 +1,5 @@
+# Exercise 4
+
+1. Move the array of cars to `index.js`. Pass the array of cars as props to Car Tool.
+
+2. Ensure it works!
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,9 +3,19 @@ import { render } from 'react-dom';
 import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
+const colorList = [
+  { id: 1, name: 'blue', hexcode: '0000ff' }, // object literal
+  { id: 2, name: 'red', hexcode: 'ff0000' },
+  { id: 3, name: 'black', hexcode: '000000' },
+  { id: 4, name: 'purple', hexcode: 'ff00ff' },
+  { id: 5, name: 'white', hexcode: 'ffffff' },
+  { id: 6, name: 'green', hexcode: '00ff00' }
+] // array literal;
+
 // render(createElement(HelloWorld), document.querySelector('#root'));
 render(<>
-  <ColorTool />
+  {/* createElement(ColorTool, { colors: colorList }) */}
+  <ColorTool colors={colorList} />
   <CarTool />
 </>, document.querySelector('#root'));
 
