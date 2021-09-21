49c8d3354f96a60cd91ad8d1697bad93f9cf7d04
Eric Greene <eric@t4d.io>
2021-09-21 08:41:49 -0400

    add solution to day 1

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -1,9 +1,111 @@
+import { useState } from "react";
 
+export const CarTool = (props) => {
 
-export const CarTool = () => {
+  const [
+    carForm,
+    setCarForm,
+  ] = useState({
+    make: '',
+    model: '',
+    year: 1900,
+    color: '',
+    price: 0,
+  });
+
+  const change = e => {
+    setCarForm({
+      ...carForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(carForm);
+
   return (
-    <header>
-      <h1>Car Tool</h1>
-    </header>
+    <>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Car</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.cars.map((car) => (
+            <tr key={car.id}>
+              <td>{car.id}</td>
+              <td>{car.make}</td>
+              <td>{car.model}</td>
+              <td>{car.year}</td>
+              <td>{car.color}</td>
+              <td>{car.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <form>
+        <div>
+          <label htmlFor="make-input">Make</label>
+          <input
+            type="text"
+            id="make-input"
+            value={carForm.make}
+            onChange={change}
+            name="make"
+          />
+        </div>
+        <div>
+          <label htmlFor="model-input">Model</label>
+          <input
+            type="text"
+            id="model-input"
+            value={carForm.model}
+            onChange={change}
+            name="model"
+          />
+        </div>
+        <div>
+          <label htmlFor="year-input">Year</label>
+          <input
+            type="text"
+            id="year-input"
+            value={carForm.year}
+            onChange={change}
+            name="year"
+          />
+        </div>
+        <div>
+          <label htmlFor="color-input">Color</label>
+          <input
+            type="text"
+            id="color-input"
+            value={carForm.color}
+            onChange={change}
+            name="color"
+          />
+        </div>
+        <div>
+          <label htmlFor="price-input">Price</label>
+          <input
+            type="text"
+            id="price-input"
+            value={carForm.price}
+            onChange={change}
+            name="price"
+          />
+        </div>
+        <button type="button">
+          Add Car
+        </button>
+      </form>
+    </>
   );
 };
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -4,19 +4,36 @@ import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
 const colorList = [
-  { id: 1, name: 'blue', hexcode: '0000ff' }, // object literal
+  { id: 1, name: 'blue', hexcode: '0000ff' },
   { id: 2, name: 'red', hexcode: 'ff0000' },
   { id: 3, name: 'black', hexcode: '000000' },
   { id: 4, name: 'purple', hexcode: 'ff00ff' },
   { id: 5, name: 'white', hexcode: 'ffffff' },
   { id: 6, name: 'green', hexcode: '00ff00' }
-] // array literal;
+];
 
-// render(createElement(HelloWorld), document.querySelector('#root'));
+const carList = [
+  {
+    id: 1,
+    make: "Ford",
+    model: "Fusion Hybrid",
+    year: 2021,
+    color: "blue",
+    price: 45000,
+  },
+  {
+    id: 2,
+    make: "Tesla",
+    model: "S",
+    year: 2019,
+    color: "white",
+    price: 120000,
+  },
+];
+
 render(<>
-  {/* createElement(ColorTool, { colors: colorList }) */}
   <ColorTool colors={colorList} />
-  <CarTool />
+  <CarTool cars={carList} />
 </>, document.querySelector('#root'));
 
 
