29c6a1007a16a949f9ea7791ae63cf69293c3560
Eric Greene <eric@t4d.io>
2021-09-23 20:03:55 -0400

    add code demos

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
+
+
 import './App.css';
 
-function App() {
+import { ColorTool } from './components/ColorTool';
+import { CarTool } from './components/CarTool';
+
+const colorList = [
+  { id: 1, name: 'blue', hexcode: '0000ff' },
+  { id: 2, name: 'red', hexcode: 'ff0000' },
+  { id: 3, name: 'black', hexcode: '000000' },
+  { id: 4, name: 'purple', hexcode: 'ff00ff' },
+  { id: 5, name: 'white', hexcode: 'ffffff' },
+  { id: 6, name: 'green', hexcode: '00ff00' }
+];
+
+const carList = [
+  {
+    id: 1,
+    make: "Ford",
+    model: "Fusion Hybrid",
+    year: 2021,
+    color: "blue",
+    price: 45000,
+  },
+  {
+    id: 2,
+    make: "Tesla",
+    model: "S",
+    year: 2019,
+    color: "white",
+    price: 120000,
+  },
+];
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header id="page-header">
+        <h1>The Tools</h1>
       </header>
+      <nav id="menubar">
+        <ul>
+          <li className="menuitem"><Link to="/">Home</Link></li>
+          <li className="menuitem"><Link to="/color-tool">Color Tool</Link></li>
+          <li className="menuitem"><Link to="/car-tool">Car Tool</Link></li>
+        </ul>
+      </nav>
+      <main id="content">
+        <Switch>
+          <Route path="/" exact><h2>Home</h2></Route>
+          <Route path="/color-tool"><ColorTool colors={colorList} /></Route>
+          <Route path="/car-tool"><CarTool cars={carList} /></Route>
+        </Switch>
+      </main>
+      <aside id="sidebar">
+        Sidebar
+      </aside>
+      <footer id="page-footer">
+        <small>Footer</small>
+      </footer>
     </div>
   );
 }
-
-export default App;
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,52 +1,11 @@
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ColorTool } from './components/ColorTool';
-import { CarTool } from './components/CarTool';
-
-const colorList = [
-  { id: 1, name: 'blue', hexcode: '0000ff' },
-  { id: 2, name: 'red', hexcode: 'ff0000' },
-  { id: 3, name: 'black', hexcode: '000000' },
-  { id: 4, name: 'purple', hexcode: 'ff00ff' },
-  { id: 5, name: 'white', hexcode: 'ffffff' },
-  { id: 6, name: 'green', hexcode: '00ff00' }
-];
-
-const carList = [
-  {
-    id: 1,
-    make: "Ford",
-    model: "Fusion Hybrid",
-    year: 2021,
-    color: "blue",
-    price: 45000,
-  },
-  {
-    id: 2,
-    make: "Tesla",
-    model: "S",
-    year: 2019,
-    color: "white",
-    price: 120000,
-  },
-];
-
+import { App } from './App';
 
 render(<>
   <Router>
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/color-tool">Color Tool</Link></li>
-        <li><Link to="/car-tool">Car Tool</Link></li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route path="/" exact><h2>Home</h2></Route>
-      <Route path="/color-tool"><ColorTool colors={colorList} /></Route>
-      <Route path="/car-tool"><CarTool cars={carList} /></Route>
-    </Switch>
+    <App />
   </Router>
 </>, document.querySelector('#root'));
 
