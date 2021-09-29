c4b63e1475ce735376e0e153c880fd8076af967e
Eric Greene <eric@t4d.io>
2021-09-29 14:34:20 -0400

    add exercise 1

--- apollo-app/server/src/resolvers.mjs
+++ apollo-app/server/src/resolvers.mjs
@@ -5,5 +5,27 @@ export const resolvers = {
     message() {
       return 'Welcome to React and Apollo!';
     },
+    colors() {
+      return [
+        'red', 'blue', 'green'
+      ];
+    },
+    person() {
+      return {
+        id: 1,
+        firstName: 'Bob',
+        lastName: 'Smith',
+        age: 21,
+      };
+    },
+    // authors() {
+    //   return fetch('http://localhost:5050/authors')
+    //     .then(res => res.json());
+    // },
+    async authors() {
+      const res = await fetch('http://localhost:5050/authors');
+      const authors = await res.json();
+      return authors;
+    }
   },
 };
--- apollo-app/server/src/typeDefs.mjs
+++ apollo-app/server/src/typeDefs.mjs
@@ -1,7 +1,27 @@
 import { gql } from 'apollo-server-express';
 
+// gql is a tagged template literal
 export const typeDefs = gql`
   type Query {
     message: String
+    colors: [String]
+    person: Person
+    authors: [Author]
+  }
+
+  type Person {
+    id: ID
+    firstName: String
+    lastName: String
+    age: Int
+  }
+
+  type Author {
+    id: ID
+    firstName: String
+    lastName: String
+    phoneNumber: String
   }
 `;
+
+
